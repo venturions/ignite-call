0e80b1b86cfa7b8d67a72310a851957b6a2a7609
venturions <alex.eduardo10@hotmail.com>
2023-06-08 20:58:30 -0300

    Fluxo da confirmação

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,6 +1,21 @@
+import { useState } from 'react'
 import { CalendarStep } from './CalendarStep'
-// import { ConfirmStep } from './ConfirmStep'
+import { ConfirmStep } from './ConfirmStep'
 
 export function ScheduleForm() {
-  return <CalendarStep></CalendarStep>
+  const [selectedDateTime, setSelectedDateTime] = useState<Date | null>()
+
+  function handleClearSelectedDateTime() {
+    setSelectedDateTime(null)
+  }
+
+  if (selectedDateTime) {
+    return (
+      <ConfirmStep
+        onCancelConfirmation={handleClearSelectedDateTime}
+        schedulingDate={selectedDateTime}
+      />
+    )
+  }
+  return <CalendarStep onSelectDateTime={setSelectedDateTime} />
 }
