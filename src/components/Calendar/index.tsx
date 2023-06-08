d8c1e6256d64da3a67918efc417ae086a6ee8ab0
venturions <alex.eduardo10@hotmail.com>
2023-06-08 15:12:19 -0300

    funcionalidade de nextMonth e previousMonth

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -8,20 +8,40 @@ import {
   CalendarTitle,
 } from './styles'
 import { getWeekDays } from '../../utils/get-week-days'
+import { useState } from 'react'
+import dayjs from 'dayjs'
 
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set('date', 1)
+  })
+
+  function handlePreviousMonth() {
+    const previousMonthDate = currentDate.subtract(1, 'month')
+
+    setCurrentDate(previousMonthDate)
+  }
+
+  function handleNextMonth() {
+    const previousNextDate = currentDate.add(1, 'month')
+
+    setCurrentDate(previousNextDate)
+  }
+
   const shortWeekDays = getWeekDays({ short: true })
+  const currentMonth = currentDate.format('MMMM')
+  const currentYear = currentDate.format('YYYY')
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Junho <span>2023</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviousMonth} title="Previous month">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title="Next month">
             <CaretRight />
           </button>
         </CalendarActions>
--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,3 +1,4 @@
+import '../lib/dayjs.ts'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { globalStyles } from '../styles/global'
